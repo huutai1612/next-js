58a1979e1da47478620155f5c6a46a9448eb2402
huutai1612 <tai.huu1612@gmail.com>
2021-11-15 10:28:11 +0700

    this is how we can fetch data with static generation approach in next js

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,21 @@ const DUMMY_LIST = [
 ];
 
 const HomePage = (props) => {
-	return <MeetupList meetups={DUMMY_LIST} />;
+	return <MeetupList meetups={props.meetups} />;
 };
+
+export async function getStaticProps() {
+	// In here we can fetching any data
+	// And after fetching data we can pass it to this component via props
+	// But we can only use this method to approach to static site rendering of next only in this pages folder
+	// Example :fetching some data and this fetch need await the data to return
+	// This function must return an object have key props
+	// And this props must be an object. This object will pass data into component via props
+	return {
+		props: {
+			meetups: DUMMY_LIST,
+		},
+	};
+}
 
 export default HomePage;
